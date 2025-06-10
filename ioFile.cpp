522033b21f2380c61360cc7f69a0cf5fbfbc4d83
rixkifadil@gmail.com <rixkifadil@gmail.com>
2025-06-10 17:28:57 +0700

    add stirng baris dan outfile

--- ioFile.cpp
+++ ioFile.cpp
@@ -2,3 +2,14 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int main(){
+    string baris;
+    //membuka filr dalam mode menulis.
+    ofstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open("contohfile.txt");
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+    //enlimited loop untuk menulis
+    
+}
