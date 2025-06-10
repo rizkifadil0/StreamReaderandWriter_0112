7a8901f86af49414b1b3fe74793d492a59322f59
rixkifadil@gmail.com <rixkifadil@gmail.com>
2025-06-10 18:36:11 +0700

    add pesan array 3 elemen

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -6,5 +6,10 @@
 using namespace std;
 
 int main(){
-    
+    cout << "Awal program" << endl; // penanda 1:...
+    try {
+        array<int, 3> data = { 10, 20, 30};
+        //pesan array integer 3 elemen
+        cout << data.at(5) << endl;
+    }
 }
