284bb8d5a1446ded29715b9fe3024ce9b99cf76e
rixkifadil@gmail.com <rixkifadil@gmail.com>
2025-06-10 18:19:20 +0700

    add isi while, untuk meloop input menu

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -16,6 +16,12 @@ int main() {
     //unlimited loop untuk menulis
     while (true) {
         cout << "- " ;
-        
+        //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukkan karakter q
+        if (baris == "q") break;
+        //menulis dan memasukkan nilai dari baris ke dalam file
+        outfile << baris << endl;
     }
+    
 }
