eb84a047e036b56ec4fff8d5235300224a9d29cb
rixkifadil@gmail.com <rixkifadil@gmail.com>
2025-06-10 18:12:44 +0700

    add ofstream outfile (untuk membuka file)

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -4,5 +4,12 @@
 using namespace std;
 
 int main() {
+    string baris;
+    string NamaFile;
+    cout << "masukkan Nama File : ";
+    cin >> NamaFile;
+    // membuka file dalam mode menulis
+    ofstream outfile;
     
+
 }
