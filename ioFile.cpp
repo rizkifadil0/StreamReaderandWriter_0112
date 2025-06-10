5ccc922ad3b8e848bbb4dc51bfc71104150b502e
rixkifadil@gmail.com <rixkifadil@gmail.com>
2025-06-10 17:34:21 +0700

    add close outfile

--- ioFile.cpp
+++ ioFile.cpp
@@ -11,5 +11,18 @@ int main(){
     outfile.open("contohfile.txt");
     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
     //enlimited loop untuk menulis
+    while (true) {
+        cout << "- ";
+        //menempatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //loop akan berhenti jika anda masukkan karakter q
+        if (baris == "q") break;
+        //menulis dan memasukkan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
+    }
+    // selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+    //membuka file dalam dalam moe mebaca
+    ifstream infile;
     
 }
